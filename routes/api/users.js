62e9444cd3ab0752227f37253359db82283f9429
shray tyagi <shraytyagi1408@gmail.com>
2021-03-12 23:08:33 +0530

    User Authentication

--- routes/api/users.js
+++ routes/api/users.js
@@ -4,6 +4,7 @@ const gravatar = require('gravatar');
 const bcrypt = require('bcryptjs');
 const jwt = require('jsonwebtoken');
 const keys = require('../../config/keys');
+const passport = require('passport');
 
 const User = require('../../models/User');
 
@@ -80,6 +81,16 @@ router.post('/login', (req, res) => {
                         return res.status(400).json({password: 'Password Incorrect'});
                     }
                 })
+        });
+});
+
+//@route GET api/users/current
+//Return curr user
+router.get('/current', passport.authenticate('jwt', { session: false}), (req, res) => {
+        res.json({
+            id: req.user.id,
+            name: req.user.name,
+            email: req.user.email
         });
 });
 
