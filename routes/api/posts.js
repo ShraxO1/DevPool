d810feb105bd92b4e1aeff6b05a9fce0bdd6a2b8
shray tyagi <shraytyagi1408@gmail.com>
2021-03-16 20:34:31 +0530

    Posts routes

--- routes/api/posts.js
+++ routes/api/posts.js
@@ -1,7 +1,42 @@
 const express = require('express');
-
 const router = express.Router();
+const passport = require('passport');
+const mongoose = require('mongoose');
+
+// Post model
+const Post = require('../../models/Post');
+// Profile model
+const Profile = require('../../models/Profile');
+
+// Validation
+const validatePostInput = require('../../validation/post');
 
 router.get('/test', (req, res) => res.json({msg: 'Posts Works'}));
+
+
+// @route   POST api/posts
+// @desc    Create post
+// @access  Private
+router.post(
+    '/',
+    passport.authenticate('jwt', { session: false }),
+    (req, res) => {
+        const {errors, isValid} = validatePostInput(req.body);
+
+        // Check Validation
+        if (!isValid) {
+            // Return any errors with 400 status
+            return res.status(400).json(errors);
+        }
+
+        const newPost = new Post({
+            text: req.body.text,
+            name: req.body.name,
+            avatar: req.body.avatar,
+            user: req.user.id
+        });
+
+        newPost.save().then(post => res.json(post));
+    });
 
 module.exports = router;
