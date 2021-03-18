babda264d764c42a153bca4e79180ab7187298bc
shray tyagi <shraytyagi1408@gmail.com>
2021-03-18 23:24:35 +0530

    React Router

--- client/src/App.js
+++ client/src/App.js
@@ -1,14 +1,23 @@
 import './App.css';
+import { BrowserRouter as Router, Route } from "react-router-dom";
 import Navbar from './components/layout/Navbar';
 import Landing from './components/layout/Landing';
 import Footer from './components/layout/Footer';
+import Register from "./components/auth/register";
+import Login from "./components/auth/login";
 
 function App() {
   return (
     <div className="App">
-        <Navbar />
-        <Landing />
-        <Footer/>
+        <Router>
+            <Navbar />
+            <Route exact path='/' component={Landing} />
+            <div className="container">
+                <Route exact path='/register' component={Register}/>
+                <Route exact path='/login' component={Login}/>
+            </div>
+            <Footer/>
+        </Router>
     </div>
   );
 }
--- client/src/components/layout/Landing.js
+++ client/src/components/layout/Landing.js
@@ -1,31 +1,32 @@
 import React, { Component } from 'react';
+import { Link } from 'react-router-dom';
 
 class Landing extends Component {
   render() {
     return (
-      <div className="landing">
-        <div className="dark-overlay landing-inner text-light">
-          <div className="container">
-            <div className="row">
-              <div className="col-md-12 text-center">
-                <h1 className="display-3 mb-4">Developers Pool</h1>
-                <p className="lead">
-                  {' '}
-                  Create a developer profile/portfolio, share posts and get recognized by top recruiters
-                  and connect with other developers
-                </p>
-                <hr />
-                <a href="register.html" className="btn btn-lg btn-info mr-2">
-                  Sign Up
-                </a>
-                <a href="login.html" className="btn btn-lg btn-light">
-                  Login
-                </a>
+        <div className="landing">
+          <div className="dark-overlay landing-inner text-light">
+            <div className="container">
+              <div className="row">
+                <div className="col-md-12 text-center">
+                  <h1 className="display-3 mb-4">Developer Connector</h1>
+                  <p className="lead">
+                    {' '}
+                    Create a developer profile/portfolio, share posts and get help
+                    from other developers
+                  </p>
+                  <hr />
+                  <Link to="/register" className="btn btn-lg btn-info mr-2">
+                    Sign Up
+                  </Link>
+                  <Link to="/login" className="btn btn-lg btn-light">
+                    Login
+                  </Link>
+                </div>
               </div>
             </div>
           </div>
         </div>
-      </div>
     );
   }
 }
