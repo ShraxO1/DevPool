28b9939bbcea8e4ff90cd0f73147f85f6cf24d65
shray tyagi <shraytyagi1408@gmail.com>
2021-03-23 22:52:32 +0530

    Redux

--- client/src/App.js
+++ client/src/App.js
@@ -3,7 +3,7 @@ import { BrowserRouter as Router, Route } from "react-router-dom";
 
 import jwt_decode from "jwt-decode";
 import setAuthToken from "./utils/setAuthToken";
-import {setCurrentUser} from "./actions/authActions";
+import {setCurrentUser, logoutUser} from "./actions/authActions";
 
 import Navbar from './components/layout/Navbar';
 import Landing from './components/layout/Landing';
@@ -21,6 +21,17 @@ if (localStorage.jwtToken) {
     const decoded = jwt_decode(localStorage.jwtToken);
     // Set user and isAuthenticated
     store.dispatch(setCurrentUser(decoded));
+
+    // Check for expired token
+    const currentTime = Date.now() / 1000;
+    if (decoded.exp < currentTime) {
+        // Logout user
+        store.dispatch(logoutUser());
+        // TODO: Clear current Profile
+
+        // Redirect to login
+        window.location.href = '/login';
+    }
 }
 
 function App() {
