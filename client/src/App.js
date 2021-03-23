c0eb05cd3657b1e7829d0c5e29f44e74914dbe24
shray tyagi <shraytyagi1408@gmail.com>
2021-03-23 21:38:43 +0530

    Redux

--- client/src/App.js
+++ client/src/App.js
@@ -1,12 +1,27 @@
-import './App.css';
 import store from "./store";
 import { BrowserRouter as Router, Route } from "react-router-dom";
+
+import jwt_decode from "jwt-decode";
+import setAuthToken from "./utils/setAuthToken";
+import {setCurrentUser} from "./actions/authActions";
+
 import Navbar from './components/layout/Navbar';
 import Landing from './components/layout/Landing';
 import Footer from './components/layout/Footer';
 import Register from "./components/auth/register";
 import Login from "./components/auth/login";
 import {Provider} from "react-redux";
+import './App.css';
+
+// Check for token
+if (localStorage.jwtToken) {
+    // Set auth token header auth
+    setAuthToken(localStorage.jwtToken);
+    // Decode token and get user info and exp
+    const decoded = jwt_decode(localStorage.jwtToken);
+    // Set user and isAuthenticated
+    store.dispatch(setCurrentUser(decoded));
+}
 
 function App() {
   return (
