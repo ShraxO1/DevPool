c89697775047296d51e0e8ffeb42f47ac6695f82
shray tyagi <shraytyagi1408@gmail.com>
2021-04-04 21:34:50 +0530

    Profile

--- client/src/components/dashboard/Dashboard.js
+++ client/src/components/dashboard/Dashboard.js
@@ -2,13 +2,17 @@ import React, { Component } from 'react';
 import { Link } from 'react-router-dom';
 import PropTypes from 'prop-types';
 import { connect } from 'react-redux';
-import { getCurrentProfile } from '../../actions/profileActions';
+import { getCurrentProfile, deleteAccount } from '../../actions/profileActions';
 import Spinner from '../common/Spinner';
 import ProfileActions from "./ProfileActions";
 
 class Dashboard extends Component {
     componentDidMount() {
         this.props.getCurrentProfile();
+    }
+
+    onDeleteClick(e) {
+        this.props.deleteAccount();
     }
 
     render() {
@@ -29,6 +33,12 @@ class Dashboard extends Component {
                         </p>
                         <ProfileActions />
                         <div style={{ marginBottom: '60px' }} />
+                        <button
+                            onClick={this.onDeleteClick.bind(this)}
+                            className="btn btn-danger"
+                        >
+                            Delete My Account
+                        </button>
                     </div>
                 );
             } else {
@@ -62,6 +72,7 @@ class Dashboard extends Component {
 
 Dashboard.propTypes = {
     getCurrentProfile: PropTypes.func.isRequired,
+    deleteAccount: PropTypes.func.isRequired,
     auth: PropTypes.object.isRequired,
     profile: PropTypes.object.isRequired
 };
@@ -71,4 +82,4 @@ const mapStateToProps = state => ({
     auth: state.auth
 });
 
-export default connect(mapStateToProps, { getCurrentProfile })(Dashboard);
+export default connect(mapStateToProps, { getCurrentProfile, deleteAccount })(Dashboard);
