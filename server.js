aa99a4891f20e34b7eb7107b8fdf5a9982b43807
shray tyagi <shraytyagi1408@gmail.com>
2021-04-13 22:17:59 +0530

    Ready for deployment

--- server.js
+++ server.js
@@ -30,5 +30,15 @@ app.use('/api/users', users);
 app.use('/api/profile', profile);
 app.use('/api/posts', posts);
 
+//Server static assets if in production
+if(process.env.NODE.ENV ==='production'){
+    //set static folder
+    app.use(express.static('client/build'));
+
+    app.get((req, res) => {
+       res.sendFile(path.resolve(__dirname, 'client', 'build', 'index.html'));
+    });
+}
+
 const port = process.env.PORT || 5000;
 app.listen(port, ()=> console.log(`Process running on port ${port}`));
