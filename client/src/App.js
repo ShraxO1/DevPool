ba66029c715dde843eed72caa1ee1f189d49f82e
shray tyagi <shraytyagi1408@gmail.com>
2021-04-13 12:02:37 +0530

    Profile

--- client/src/App.js
+++ client/src/App.js
@@ -1,27 +1,29 @@
-import store from "./store";
-import { BrowserRouter as Router, Route,Switch } from "react-router-dom";
+import React, { Component } from 'react';
+import { BrowserRouter as Router, Route, Switch } from 'react-router-dom';
+import jwt_decode from 'jwt-decode';
+import setAuthToken from './utils/setAuthToken';
+import { setCurrentUser, logoutUser } from './actions/authActions';
+import { clearCurrentProfile } from './actions/profileActions';
 
-import jwt_decode from "jwt-decode";
-import setAuthToken from "./utils/setAuthToken";
-import {setCurrentUser, logoutUser} from "./actions/authActions";
-import {clearCurrentProfile} from "./actions/profileActions";
+import { Provider } from 'react-redux';
+import store from './store';
 
-import PrivateRoute from "./components/common/PrivateRoute";
+import PrivateRoute from './components/common/PrivateRoute';
 
-import Dashboard from "./components/dashboard/Dashboard";
-import CreateProfile from "./components/create-profile/CreateProfile";
-import EditProfile from "./components/edit-profile/EditProfile";
-import AddEducation from "./components/add-credentials/AddEducation";
-import AddExperience from "./components/add-credentials/AddExperience";
-import Profiles from "./components/profiles/Profiles";
-import NotFound from "./components/not-found/NotFound";
-import Profile from "./components/profile/Profile";
 import Navbar from './components/layout/Navbar';
-import Landing from './components/layout/Landing';
 import Footer from './components/layout/Footer';
+import Landing from './components/layout/Landing';
 import Register from "./components/auth/register";
-import Login from "./components/auth/login";
-import {Provider} from "react-redux";
+import Login from './components/auth/login';
+import Dashboard from './components/dashboard/Dashboard';
+import CreateProfile from './components/create-profile/CreateProfile';
+import EditProfile from './components/edit-profile/EditProfile';
+import AddExperience from './components/add-credentials/AddExperience';
+import AddEducation from './components/add-credentials/AddEducation';
+import Profiles from './components/profiles/Profiles';
+import Profile from './components/profile/Profile';
+import NotFound from './components/not-found/NotFound';
+
 import './App.css';
 
 // Check for token
@@ -45,40 +47,58 @@ if (localStorage.jwtToken) {
     }
 }
 
-function App() {
-  return (
-    <div className="App">
-        <Provider store={store}>
-            <Router>
-                <Navbar />
-                <Route exact path='/' component={Landing} />
-                <div className="container">
-                    <Route exact path='/register' component={Register}/>
-                    <Route exact path='/login' component={Login}/>
-                    <Route exact path='/profiles' component={Profiles}/>
-                    <Route exact path='/:handle' component={Profile}/>
-                    <Switch>
-                    <PrivateRoute exact path='/dashboard' component={Dashboard}/>
-                    </Switch>
-                    <Switch>
-                        <PrivateRoute exact path='/create-profile' component={CreateProfile}/>
-                    </Switch>
-                    <Switch>
-                        <PrivateRoute exact path='/edit-profile' component={EditProfile}/>
-                    </Switch>
-                    <Switch>
-                        <PrivateRoute exact path='/add-experience' component={AddExperience}/>
-                    </Switch>
-                    <Switch>
-                        <PrivateRoute exact path='/add-education' component={AddEducation}/>
-                    </Switch>
-                    <Route exact path='/not-found' component={NotFound}/>
-                </div>
-                <Footer/>
-            </Router>
-        </Provider>
-    </div>
-  );
+class App extends Component {
+    render() {
+        return (
+            <Provider store={store}>
+                <Router>
+                    <div className="App">
+                        <Navbar />
+                        <Route exact path="/" component={Landing} />
+                        <div className="container">
+                            <Route exact path="/register" component={Register} />
+                            <Route exact path="/login" component={Login} />
+                            <Route exact path="/profiles" component={Profiles} />
+                            <Route exact path="/profile/:handle" component={Profile} />
+                            <Switch>
+                                <PrivateRoute exact path="/dashboard" component={Dashboard} />
+                            </Switch>
+                            <Switch>
+                                <PrivateRoute
+                                    exact
+                                    path="/create-profile"
+                                    component={CreateProfile}
+                                />
+                            </Switch>
+                            <Switch>
+                                <PrivateRoute
+                                    exact
+                                    path="/edit-profile"
+                                    component={EditProfile}
+                                />
+                            </Switch>
+                            <Switch>
+                                <PrivateRoute
+                                    exact
+                                    path="/add-experience"
+                                    component={AddExperience}
+                                />
+                            </Switch>
+                            <Switch>
+                                <PrivateRoute
+                                    exact
+                                    path="/add-education"
+                                    component={AddEducation}
+                                />
+                            </Switch>
+                            <Route exact path="/not-found" component={NotFound} />
+                        </div>
+                        <Footer />
+                    </div>
+                </Router>
+            </Provider>
+        );
+    }
 }
 
 export default App;
--- client/src/components/profile/ProfileGithub.js
+++ client/src/components/profile/ProfileGithub.js
@@ -22,6 +22,12 @@ class ProfileGithub extends Component {
             `https://api.github.com/users/${username}/repos?per_page=${count}&sort=${sort}&client_id=${clientId}&client_secret=${clientSecret}`
         )
             .then(res => res.json())
+            .then(data => {
+                if (this.refs.myRef) {
+                    this.setState({ repos: data });
+                }
+            })
+            .catch(err => console.log(err));
     }
 
     render() {
