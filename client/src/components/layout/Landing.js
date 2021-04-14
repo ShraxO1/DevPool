75719d75a1194638f1826bc5d8335b99fe9262dd
shray tyagi <shraytyagi1408@gmail.com>
2021-04-14 19:53:36 +0530

    Ready for deployment

--- client/src/components/layout/Landing.js
+++ client/src/components/layout/Landing.js
@@ -17,11 +17,11 @@ class Landing extends Component {
             <div className="container">
               <div className="row">
                 <div className="col-md-12 text-center">
-                  <h1 className="display-3 mb-4">Developer Connector</h1>
+                  <h1 className="display-3 mb-4">Developers Hub</h1>
                   <p className="lead">
                     {' '}
-                    Create a developer profile/portfolio and get noticed by recruiters,
-                    connect with other developers
+                    Create a developer profile/portfolio, connect with other developers
+                    and get noticed by recruiters
                   </p>
                   <hr />
                   <Link to="/register" className="btn btn-lg btn-info mr-2">
